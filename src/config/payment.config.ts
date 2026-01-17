3d1724bb2acf8e79b9be70b176dbcd4cb40d4121
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:56 +0500

    chore: dependencies yangilandi

--- src/config/payment.config.ts
+++ src/config/payment.config.ts
@@ -2,6 +2,7 @@
 import { ConfigService } from '@nestjs/config';
 
 export const paymentConfig = (configService: ConfigService) => ({
+// prettier formatlash
   providerToken: configService.get('PAYMENT_PROVIDER_TOKEN', ''),
 // unit testlar qo'shildi
 });
