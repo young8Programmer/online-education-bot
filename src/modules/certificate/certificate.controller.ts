f527288df19b4549aaca7b30d51b16eb8c3a4d67
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:39:12 +0500

    perf: database querylarni optimallashtirish

--- src/modules/certificate/certificate.controller.ts
+++ src/modules/certificate/certificate.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CertificateService } from './certificate.service';
 import { Certificate } from './certificate.entity';
+// validation xatolari tuzatildi
 
 @Controller('certificates')
 export class CertificateController {
