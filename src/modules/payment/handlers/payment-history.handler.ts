8cda1729809047544ed9facc6339c28d7b443966
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:40:07 +0500

    fix: database connection muammosi hal qilindi

--- src/modules/payment/handlers/payment-history.handler.ts
+++ src/modules/payment/handlers/payment-history.handler.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PaymentService } from '../payment.service';
+// kod strukturasini yaxshilash
 // ESLint qoidalariga moslashtirish
 import { UserService } from '../../user/user.service';
 import { I18nService } from '../../i18n/i18n.service';
