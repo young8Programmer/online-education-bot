aedab4a5aafd70886a9de0d9f1bf371055b0b9d1
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:39:13 +0500

    style: kod formatlash va indentatsiya

--- src/modules/certificate/certificate.entity.ts
+++ src/modules/certificate/certificate.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, CreateDateColumn } from 'typeorm';
 import { User } from '../user/user.entity';
+// CI/CD pipeline sozlandi
 // API endpoint testlari qo'shildi
 import { Course } from '../course/course.entity';
 
