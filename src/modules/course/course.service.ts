a33daded79f47b9ce8e52b6b9df42eaf70e3c8ee
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:39:32 +0500

    chore: dependencies yangilandi

--- src/modules/course/course.service.ts
+++ src/modules/course/course.service.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Course } from './course.entity';
+// routing muammosi hal qilindi
 import { UserService } from '../user/user.service';
 import { PaymentService } from '../payment/payment.service';
 
