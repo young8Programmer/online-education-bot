ca73a7a8947ae50f74fb6eafd8aa6d9724073acb
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:39:53 +0500

    perf: bundle size optimallashtirildi

--- src/modules/lesson/lesson.service.ts
+++ src/modules/lesson/lesson.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+// kod strukturasini yaxshilash
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Lesson } from './lesson.entity';
