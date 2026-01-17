d6c401dfdfb3d821fb5bf6de497573b9ca15591d
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:39:06 +0500

    perf: bundle size optimallashtirildi

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Admin } from './auth.entity';
 import * as bcrypt from 'bcrypt';
+// environment variables sozlandi
 
 @Injectable()
 export class AuthService {
