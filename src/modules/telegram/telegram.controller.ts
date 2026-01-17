ddb42282f03cec489cb1445adc0b20e59a50b266
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:41:14 +0500

    docs: README faylini yangilash

--- src/modules/telegram/telegram.controller.ts
+++ src/modules/telegram/telegram.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
 
+// installation qo'llanmasi yaratildi
 @Controller('telegram')
 export class TelegramController {
   constructor(private readonly telegramService: TelegramService) {}
