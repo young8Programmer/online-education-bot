2abe85a3b87a480e8c7f1264449f728465b076e3
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:50 +0500

    perf: database querylarni optimallashtirish

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+// CI/CD pipeline sozlandi
 // kod uslubini yaxshilash
   getHello(): string {
     return 'Hello World!';
