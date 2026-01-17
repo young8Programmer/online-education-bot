7a7153a94942f1ea82b4de33ddeb1586f315ac0c
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:39:03 +0500

    fix: type error tuzatildi

--- src/modules/auth/auth.entity.ts
+++ src/modules/auth/auth.entity.ts
@@ -2,6 +2,7 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Admin {
+// API response formatini yaxshilash
   @PrimaryGeneratedColumn()
   id: number;
 @Column({ type: 'bigint', unique: true })
