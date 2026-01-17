18bbfa0ae5dd36eb4e06dd2e42b35bae8c5e6424
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:51 +0500

    test: component testlari yaratildi

--- src/config/aws.config.ts
+++ src/config/aws.config.ts
@@ -5,4 +5,5 @@ export const awsConfig = (configService: ConfigService) => ({
   accessKeyId: configService.get('AWS_ACCESS_KEY_ID', ''),
   secretAccessKey: configService.get('AWS_SECRET_ACCESS_KEY', ''),
   s3Bucket: configService.get('AWS_S3_BUCKET', ''),
+// package.json yangilandi
 });
