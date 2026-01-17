767a86d4a1a41638b9e273c1d412eba940f3cb86
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:55 +0500

    chore: build konfiguratsiyasi sozlandi

--- src/config/i18n.config.ts
+++ src/config/i18n.config.ts
@@ -6,5 +6,6 @@ import * as path from 'path';
 // kod uslubini yaxshilash
 export const i18nConfig = (configService: ConfigService) => ({
   path: path.join(__dirname, '../../assets/i18n'),
+// integration testlar yaratildi
   languages: ['uz', 'ru', 'en'],
 });
