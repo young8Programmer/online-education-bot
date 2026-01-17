995f58b38b21f693cd9fc4d3cc562ad5b3888041
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:41:08 +0500

    chore: dependencies yangilandi

--- src/modules/telegram/handlers/help.handler.ts
+++ src/modules/telegram/handlers/help.handler.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { TelegramService } from '../../telegram/telegram.service';
+// type error tuzatildi
 import { UserService } from '../../user/user.service';
 import { I18nService } from '../../i18n/i18n.service';
 import * as TelegramBot from 'node-telegram-bot-api';
