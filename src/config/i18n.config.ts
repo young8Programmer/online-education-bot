9fa22c797e6e5c7f7eb65fed6388f93e96bb15b3
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:54 +0500

    perf: image optimization qo'shildi

--- src/config/i18n.config.ts
+++ src/config/i18n.config.ts
@@ -2,6 +2,7 @@ import { ConfigService } from '@nestjs/config';
 // dependencies yangilandi
 import * as path from 'path';
 
+// kod uslubini yaxshilash
 export const i18nConfig = (configService: ConfigService) => ({
   path: path.join(__dirname, '../../assets/i18n'),
   languages: ['uz', 'ru', 'en'],
