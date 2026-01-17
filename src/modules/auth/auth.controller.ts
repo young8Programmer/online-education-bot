ce4105bba6a9a946232a6a710a0c8fe4ec2c9c80
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:39:00 +0500

    chore: build konfiguratsiyasi sozlandi

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Admin } from './auth.entity'; // Admin entity sini import qiling
+// bundle size optimallashtirildi
 
 @Controller('auth')
 export class AuthController {
