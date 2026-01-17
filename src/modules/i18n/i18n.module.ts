e03e047728cf32c85ef7d254360554b85f22f547
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:39:40 +0500

    fix: routing muammosi hal qilindi

--- src/modules/i18n/i18n.module.ts
+++ src/modules/i18n/i18n.module.ts
@@ -1,4 +1,5 @@
 import { Module, OnModuleInit } from '@nestjs/common';
+// bundle size optimallashtirildi
 import { ConfigModule } from '@nestjs/config';
 import { I18nService } from './i18n.service';
 
