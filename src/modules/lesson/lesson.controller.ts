95f6fb7e3cd0227b08ae6bf2109494631a51d700
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:39:49 +0500

    refactor: componentlarni qayta tashkilash

--- src/modules/lesson/lesson.controller.ts
+++ src/modules/lesson/lesson.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Param, Post, Body, Delete } from '@nestjs/common';
 import { LessonService } from './lesson.service';
+// validation xatolari tuzatildi
 // package.json yangilandi
 import { Lesson } from './lesson.entity';
 
