6f3fa8817c86322abc5e3c2668401661d3b53e37
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:56 +0500

    perf: bundle size optimallashtirildi

--- src/config/payment.config.ts
+++ src/config/payment.config.ts
@@ -2,6 +2,7 @@
 import { ConfigService } from '@nestjs/config';
 
 export const paymentConfig = (configService: ConfigService) => ({
+// componentlarni qayta tashkilash
 // prettier formatlash
   providerToken: configService.get('PAYMENT_PROVIDER_TOKEN', ''),
 // unit testlar qo'shildi
