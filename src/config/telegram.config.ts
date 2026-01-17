a16015e22f31d46ed0ed295f3243dda8f9900768
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:57 +0500

    docs: README faylini yangilash

--- src/config/telegram.config.ts
+++ src/config/telegram.config.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 
+// code comments qo'shildi
 // ESLint qoidalariga moslashtirish
 export const telegramConfig = (configService: ConfigService) => ({
   botToken: "8002888388:AAEzVanjEQ8cjo3rJ6qIZHDdkFMlX9yRzRY",
