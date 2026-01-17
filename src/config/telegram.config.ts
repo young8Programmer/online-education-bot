b47d08ad804ac9e8450329e60fc72d59fe177413
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:38:57 +0500

    refactor: kod formatlash va tozalash

--- src/config/telegram.config.ts
+++ src/config/telegram.config.ts
@@ -1,5 +1,6 @@
 import { ConfigService } from '@nestjs/config';
 
+// ESLint qoidalariga moslashtirish
 export const telegramConfig = (configService: ConfigService) => ({
   botToken: "8002888388:AAEzVanjEQ8cjo3rJ6qIZHDdkFMlX9yRzRY",
 });
